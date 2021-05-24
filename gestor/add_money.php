d715cf6999a79e4eee9f92687611080077cb77cd
rubentc99 <rubentc99@hotmail.com>
2021-05-24 23:07:08 +0200

    TFG "terminado"
    A falta de darle un par de vueltas a lo de insertar dinero, para que no se inserte varias veces.

--- gestor/add_money.php
+++ gestor/add_money.php
@@ -3,26 +3,36 @@
     require_once "clases/Bd.php";
     require_once "includes/protect.php";
 
+    //creo un objeto vacío de tipo Dinero
     $dinero = new Dinero();
 
-    //este codigo se utiliza en el caso de estemos entrando a este formulario, por el botón de editar
+    //este codigo se utiliza en el caso de querer editar el dinero
     //en tal caso, vemos si viene un id por get, y en tal caso rellenamos el formulario con los datos para que se puedan modificar
     if(isset($_GET['id']) && !empty($_GET['id'])){ //si hay algo instaciado en el post llamado id, y si no está vacío
-        $id = intval($_GET['id']); //capturo el id del objeto //el intval es para seguridad
-        $dinero->obtenerPorId($id);
+        $id = intval($_GET['id']); //capturo el id del objeto
+        $dinero->obtenerPorId($id); //relleno el objeto
     }
 
     if(isset($_POST) && !empty($_POST)){
         if(!empty($_POST['id'])){ //si viene un id por post, es porque hemos clickado en editar y queremos hacer una actualizacion
-            //actualizo
-            $id = intval($_POST['id']);
-            $dinero->actualizar($id, $_POST);
-            echo "actualiza";
+            $id = intval($_POST['id']); //asocio el id del objeto a una variable
+            $dinero->actualizar($id, $_POST); //envio a la funcion actualizar el id, y la información que viene por post
+            header('location:home.php'); //reenvio al home
         }
         $dinero->insertar($_POST, "dinero"); //inserto los datos del post
-        //header('location:home.php');
+        header('location:home.php'); //reenvio al home
+        /*
+        if($_POST['id']==0){ //si el id del post es 0 (el primer id que entrará), hago un insert, si no, no hago nada para que solo quiero que se meta 1 registro a la bbdd
+            $dinero->insertar($_POST, "dinero"); //inserto los datos del post
+            header('location:home.php'); //reenvio al home
+        }else{ //muestro un alert a forma de error, para que el usuario modifique el importe ya añadido, y le redirijo al home
+            ?><script>alert("No puedes agregar dinero porque ya hay una cantidad añadida. Debes modificar la cantidad actual.");
+                window.location='home.php'
+            </script>
+            <?php
+        }
+        */
     }
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -35,7 +45,7 @@
         include "includes/aside.php";
     ?>
     <section>
-        <div class="category_creation_div">
+        <div class="create_money_div">
             <form id="form_category" name="category" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
                 <input type="hidden" name="id" value="<?php echo $dinero->getId()?>">
                 <div class="form_category_money">
--- gestor/includes/header.php
+++ gestor/includes/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="dark_header">
-        <div class="img_box_home" style="float: left">
+        <div class="img_box_home">
             <a href="./home.php">
                 <img src="img/ez2save_blanco.png">
             </a>
